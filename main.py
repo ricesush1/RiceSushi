ea288a77188908217708e25b10a3e5e3186f0b8d
Hassan Allam <89223651+ricesush1@users.noreply.github.com>
2022-03-31 19:32:35 +0000

    submenu

--- main.py
+++ main.py
@@ -1,21 +1,81 @@
-import menuy
-
-
 main_menu = [
-  ["Swap", "Week 0/numswap.py"],
-  ["Factors", "Week 2/factors.py"],
-  ["Factorial with classes", "Week 2/classfactorial.py"],
-  ["Fibonacci", "Week 1/fibonacci.py"],
-  ["Fibonacci with classes", "Week 2/classfibonacci.py"],
-  ["Lists & Loops", "Week 1?listloop.py"]
 ]
+
+week0_list = [
+    ["Swap", "Week 0/numswap.py"],
+]
+
+week1_list = [
+
+  ["InfoDb", "Week 1/listloop.py"],
+  ["Fibonacci Imperative", "Week 1/fibonacci.py"]
+]
+
+week2_list = [
+    ["Factorial", "Week 2/classfactorial.py"],
+    ["Factors", "Week 2/factors.py"], 
+    ["Fibonacci OOP", "Week 2/classfibonacci.py"],
+]
+
 border = "=" * 25
 banner = f"\n{border}\nPlease Select An Option\n{border}"
-def menu():
-    title = "Function Menu" + banner
-    menu_list = main_menu.copy()
-    menuy.menu(title, menu_list)
 
-print(menu)
+def menu():
+  print()
+  title = "Function Menu" + banner
+  menu_list = main_menu.copy()
+  menu_list.append(["Design", week0_func])
+  menu_list.append(["Lists", week1_func])
+  menu_list.append(["Math", week2_func])
+  buildMenu(title, menu_list)
+
+def week0_func():
+  title = "Week 0" + banner
+  buildMenu(title, week0_list)
+  
+def week1_func():
+  title = "Week 1" + banner
+  buildMenu(title, week1_list)
+
+def week2_func():
+  title = "Week 2" + banner
+  buildMenu(title, week2_list)
+
+
+def buildMenu(banner, options):
+    print(banner)
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
+
+    for key, value in prompts.items():
+        print(key, '->', value[0])
+
+    choice = input("Type your choice> ")
+
+    try:
+        choice = int(choice)
+        if choice == 0:
+            return
+        try:
+            action = prompts.get(choice)[1]
+            action()
+        except TypeError:
+            try:  
+                exec(open(action).read())
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+          
+    except ValueError:
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        print(f"Invalid choice: {choice}")
+    except TypeError:
+        print(f"Not callable {action}")
+
+    buildMenu(banner, options)  
+
+
 if __name__ == "__main__":
     menu()
